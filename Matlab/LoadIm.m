cc0239a20454898307ad7ededb25e5b4ccb792c7
Sindri Magnússon <sindrim@csc.kth.se>
2012-05-03 17:18:55 +0200

    Uppdated LoadIm.m to deal with images with std = 0

--- Matlab/LoadIm.m
+++ Matlab/LoadIm.m
@@ -1,5 +1,9 @@
 function [im,ii_im] = LoadIm(im_fname)
    im = double(imread(im_fname));
-   im = (im - mean(im(:))) / std(im(:));
+   if std(im(:)) == 0
+      im = zeros(size(im));
+   else
+      im = (im - mean(im(:))) / std(im(:));
+   end
    ii_im = cumsum(cumsum(im,1),2);
 end
