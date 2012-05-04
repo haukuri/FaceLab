ece4af319ea958dd3aa10f65802061abb393cac7
Haukur Ingi Heidarsson <haukur@heidarsson.is>
2012-05-04 15:31:26 +0200

    Fixed a bug in LearnWeakClassifier.m

--- Matlab/testLearnWeakClassifier.m
+++ Matlab/testLearnWeakClassifier.m
@@ -1,11 +1,12 @@
 clear all
 [Fdata,NFdata,FTdata] = loadData();
-ys = [ones(2000,1); zeros(4000,1)];
-ftype_vec = VecFeature(FTdata.all_ftypes(12028,:), FTdata.W, FTdata.H);
+ftype_vec = FTdata.fmat(:,12028);
 fs1 = VecComputeFeature(Fdata.ii_ims, ftype_vec);
 fs2 = VecComputeFeature(NFdata.ii_ims, ftype_vec);
 fs = [fs1;fs2];
-ws = ones(6000,1)/6000;
-nanfs = isnan(fs);
-fs(nanfs) = 0;
+Nf = size(Fdata.ii_ims,1);
+Nnf = size(NFdata.ii_ims,1);
+Nt = Nf + Nnf;
+ws = ones(Nt,1)/Nt;
+ys = [ones(Nf,1); zeros(Nnf,1)];
 [theta,p,err] = LearnWeakClassifier(ws, fs, ys)
