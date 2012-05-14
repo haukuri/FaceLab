9b917135946b85c09c1cdccb1520a25c9b68f92d
Sindri <sim20@hi.is>
2012-05-14 17:18:01 +0200

    s

--- Matlab/ApplyCascade.m
+++ Matlab/ApplyCascade.m
@@ -1,5 +1,21 @@
-function [fg, sc] = ApplyCascade(CCparams, ii_im)
+function [fg, sc] = ApplyCascade(CCparams, ii_im,ii_im2)
     
+
+    ii_im = double(ii_im);
+    
+        mu = [];
+    sigma2 = [];
+    
+    if nargin == 3
+        % Cause the faces images are of size 19x19
+        ii_im2 = double(ii_im2);
+        L = 19;
+        L2 = L*L;
+    
+        mu = ComputeBoxSum(ii_im,1,1,L,L)/L2;
+        sigma2 = (ComputeBoxSum(ii_im2,1,1,L,L)-L2*mu*mu)/(L2-1);
+    end
+
     % Number of classifiers in the cascade
     N = length(CCparams);
     
@@ -15,11 +31,17 @@ function [fg, sc] = ApplyCascade(CCparams, ii_im)
         fmat = Cparams.fmat(:,J);
         theta = Cparams.Thetas(:,2);
         p = Cparams.Thetas(:,3);
+        ftypes = Cparams.all_ftypes(J,:);
         
         
         sc = 0;
         for j = 1:length(alpha)
             fs = VecComputeFeature(ii_im(:)', fmat(:,j));
+            if nargin == 3
+                w = ftypes(j,4);
+                h = ftypes(j,5);
+                fs = (1/sqrt(sigma2))*(fs+(ftypes(j,1)==3)*w*h*mu);
+            end
             sc = sc + alpha(j)*(p(j) * fs < p(j) * theta(j)); 
         end
         
